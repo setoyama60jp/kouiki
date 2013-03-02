6b4ed235a204de8c92e5c687247788bca5e7fbb4
Masato setoyama <setoyama60jp@gmail.com>
2013-03-02 23:55:01 +0900

    refactored chapter writing logic

--- lib/kouiki.rb
+++ lib/kouiki.rb
@@ -16,21 +16,7 @@ module Kouiki
 
         file_name = "chapter" + index.to_s + ".txt"
 
-        result = File.open(file_name, "w")
-
-        #タイトルを書き込み
-        result.write('# ' + chapter.elements["title"].text + "\n")
-        crlf result #改行
-        crlf result
-
-        ##とりあえず、ソースコード要素全てを抽出
-        program_listings = chapter.elements.to_a("//programlisting")
-
-        program_listings.each do |program_listing|
-          write_program_listing program_listing, result
-        end
-
-        result.close
+        write_chapter(file_name, chapter)
 
         #チャプター番号を1個増やす
         index + 1
@@ -40,6 +26,24 @@ module Kouiki
     end
 
     private
+
+    def write_chapter(file_name, chapter_element)
+      result = File.open(file_name, "w")
+
+      #タイトルを書き込み
+      result.write('# ' + chapter_element.elements["title"].text + "\n")
+      crlf result #改行
+      crlf result
+
+      ##とりあえず、ソースコード要素全てを抽出
+      program_listings = chapter_element.elements.to_a("//programlisting")
+
+      program_listings.each do |program_listing|
+        write_program_listing program_listing, result
+      end
+
+      result.close
+    end
 
     def write_program_listing(program_listing_element, file)
       file.write("```") and crlf file
