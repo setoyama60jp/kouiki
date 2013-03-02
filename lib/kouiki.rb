65acefd27f3a03982b4937d85c938a48bbf4e194
Masato setoyama <setoyama60jp@gmail.com>
2013-03-03 01:32:51 +0900

    can handle userinput element and replaceable element in programlisting element

--- lib/kouiki.rb
+++ lib/kouiki.rb
@@ -39,16 +39,29 @@ module Kouiki
       program_listings = chapter_element.elements.to_a("//programlisting")
 
       program_listings.each do |program_listing|
-        write_program_listing program_listing, result
+        write_program_listing program_listing, result, "python"
       end
 
       result.close
     end
 
-    def write_program_listing(program_listing_element, file)
+    def write_program_listing(program_listing_element, file, language = nil)
       file.write("```") and crlf file
-      file.write('#!python') and crlf file
-      file.write(program_listing_element.text + "\n")
+      file.write('#!') + file.write(language) and crlf file if language
+
+      program_listing_element.each_child do |child|
+        if (child.node_type == :text)
+          file.write(child.value)
+        else
+          if (child.node_type == :element && child.name == "userinput")
+            file.write(child.text) and crlf file
+          else
+            file.write(child.text)
+          end
+        end
+      end
+      crlf file
+
       file.write("```") and crlf file
     end
 
